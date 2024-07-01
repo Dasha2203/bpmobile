e13dc5eaa42e8b80bf5bafe46fdfec50699d7f10
Dasha2203 <44381104+Dasha2203@users.noreply.github.com>
2024-07-01 11:49:49 +0300

    feat: banner

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import './index.html'
+import './styles.css'
+import './locales/de.json'
+import './locales/en.json'
+import './locales/es.json'
+import './locales/fr.json'
+import './locales/ja.json'
+import './locales/pt.json'
+import { loadTranslations, t } from './scripts/translations'
+
+const LOCALES_CODE = ['de', 'en', 'es', 'fr', 'ja', 'pt']
+
+const urlParams = new URLSearchParams(window.location.search)
+let currentLanguage = LOCALES_CODE.includes(urlParams.get('lang')) ? urlParams.get('lang') : 'en'
+
+function updateText() {
+  document.querySelectorAll('[data-i18n]').forEach(element => {
+    const key = element.getAttribute('data-i18n');
+
+    const attrs = Object.values(element.attributes)
+      .filter(i => i.name.includes('data-i18n-var-'))
+      .map(i => i.name.replace('data-i18n-var-', ''))
+
+    const props = {}
+
+    for (let i = 0; i < attrs.length; i++) {
+      props[attrs[i]] = element.getAttribute(`data-i18n-var-${attrs[i]}`)
+    }
+
+    element.innerHTML = t(key, props);
+  });
+}
+
+loadTranslations(currentLanguage).then(() => {
+  updateText()
+})
+
+const offers = document.querySelectorAll('.offer')
+const offerSbmBtn = document.querySelector('#offer-submit')
+
+offers.forEach(offer => {
+
+  offer.addEventListener('click', () => {
+    offers.forEach(offer => {
+      offer.classList.remove('offer_active')
+    })
+
+    offer.classList.add('offer_active')
+    
+    let href = offer.getAttribute('data-href')
+    offerSbmBtn.setAttribute('href', href)
+  })
+})
---
+++ webpack.config.js
@@ -0,0 +1,126 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+
+const devMode = mode === 'development';
+
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
+
+module.exports = {
+  mode,
+  target,
+  devtool,
+  devServer: {
+    open: true,
+  },
+  entry: [
+    '@babel/polyfill',
+    path.resolve(__dirname, 'src', 'index.js'),
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    filename: "main.[contenthash].js",
+
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html')
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.json$/,
+        type: 'javascript/auto',
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'locales'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.(c|sa|sc)ss$/i,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [require('postcss-preset-env')]
+              }
+            }
+          },
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.woff2?$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/fonts/[name][ext]',
+        },
+      },
+      {
+        test: /\.(jpe?g|png|webp|gif|svg)$/i,
+        use: devMode
+          ? []
+          : [
+              {
+                loader: 'image-webpack-loader',
+                options: {
+                  mozjpeg: {
+                    progressive: true,
+                  },
+                  optipng: {
+                    enabled: false,
+                  },
+                  pngquant: {
+                    quality: [0.65, 0.9],
+                    speed: 4,
+                  },
+                  gifsicle: {
+                    interlaced: false,
+                  },
+                  webp: {
+                    quality: 75,
+                  },
+                },
+              },
+            ],
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/img/[name][ext]',
+        },
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
+    ]
+  }
+}
